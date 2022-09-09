664b9ae9aad5da43136d29a11e2803efe7b70ebf
albinlidback <albin@lidback.com>
2022-09-09 13:39:41 +0200

    first commit

---
+++ base/server/src/main/resources/webassets/hello/hello.js
@@ -0,0 +1,8 @@
+var base = base || {};
+base.helloController = function() {
+    //TODO: the controller object needs a load function
+    const controller = {
+        load: function() {}
+        };
+    return controller;
+};
